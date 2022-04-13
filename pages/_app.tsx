9bd71d67df531e33ab8662943d41468ffa7d2755
Alexandra Robison <alexandra@arobison.net>
2022-04-13 00:44:08 -0700

    provide appsettings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { AppSettingsProvider } from '../context/AppSettings';
 
 const App = ({ Component, pageProps }: AppProps) => (
-	<>
-		<Head>
-			<title>VATUSA</title>
-			<link rel="icon" type="image/png" href="/favicon.png" />
-		</Head>
-		<Component {...pageProps} />
-	</>
+	<AppSettingsProvider>
+		<>
+			<Head>
+				<title>VATUSA</title>
+				<link rel="icon" type="image/png" href="/favicon.png" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	</AppSettingsProvider>
 );
 
 export default App;
